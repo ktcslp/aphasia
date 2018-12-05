db3fedb2886bbfd0efc0080fb41d7aec3d65faa7
Kevin Cunningham <kevin_cunningham@med.unc.edu>
2018-12-04 20:39:00 -0500

    na

--- editRatio/R/editRatio.R
+++ editRatio/R/editRatio.R
@@ -4,19 +4,16 @@ df <- read.csv(file = "DIST01real.csv", header = FALSE,stringsAsFactors = FALSE)
 #'@param transcript a dataframe that contains target and produced transcription on alternating lines with each character as a separate dataframe value.
 #' @return the edit distance ratio, the number of Levenshtein edits divided by the length of the longest string
 #'@examples
-#'transcript<- data.frame("Transcription"=c("Saul", "Paul"))
+#'transcript2<- data.frame("Transcription"=c("Saul", "Paul"))
 #'editRatio(transcript)
-editRatio <- function(transcript) {
-  df <- transcript
-  df <- df[!apply(is.na(df) | df == "", 1, all),]
-  df[df=="F"]<-"t"
-  ind <- seq(3, nrow(df), by = 3)
-  dfbroad <- df[-ind,]
-  distlist <- df[ind,]
-  df.new <- (cbind(a = c("target","production"), dfbroad))
-
-#get edit distance by computing number of in  each edit class and then summing them
-  for (i in 1:nrow(df.new)){
+editRatio <- function(x) {
+  #df[!apply(df == "", 1, all),]
+  #transcript <- transcript[!apply(is.na(transcript) | transcript == "", 1, all),]
+  #x<-nrow(transcript)
+  #row.names(transcript) <- 1:x
+  df.new <- (cbind(a = c("target","production"), x))
+  #df.new <- data.frame(df.new)
+    for (i in 1:nrow(df.new)){
     if(df.new[i,1] == 'target'){
       target <- df.new[i,-1]
       production <- df.new[i+1,-1]
@@ -37,24 +34,26 @@ editRatio <- function(transcript) {
   #number of producted segments
   for (i in 1:nrow(df.new)){
     if(df.new[i,1] == 'production'){
-      segments <- nchar (df.new[i,-1])
+      segments <- paste(df.new[i,-1])
+      segments <- nchar(segments)
       write.table(segments, file = "prodsegs.csv", append = TRUE, sep = ",", row.names = FALSE, col.names = FALSE)
     }
     else {NULL}
   }
   a <- read.csv(file = "prodsegs.csv", header = FALSE,stringsAsFactors = FALSE)
-  prodnum <- sum(a>0)
+  prodnum <- sum(a)
 
   #get number of target segments
   for (i in 1:nrow(df.new)){
     if(df.new[i,1] == 'target'){
-      segments <- nchar (df.new[i,-1])
+      segments <- paste(df.new[i,-1])
+      segments <- nchar(segments)
       write.table(segments, file = "segments.csv", append = TRUE, sep = ",", row.names = FALSE, col.names = FALSE)
     }
     else {NULL}
   }
   dfdenom <- read.csv(file = "segments.csv", header = FALSE,stringsAsFactors = FALSE)
-  targetnum <- sum(dfdenom>0)
+  targetnum <- sum(dfdenom)
 
   if(prodnum>targetnum){
     editdistance <- (c("editdistance", numerator/prodnum))
